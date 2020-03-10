2132dae075bdb4f9d6b0770e17d3e1d46d94168d
Karl Smink <krlsmnk@yahoo.com>
2020-03-10 10:29:01 -0500

    PNG Player

---
+++ Assets/PNG_Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNG_Player : MonoBehaviour
+{
+     public float duration = 0.5f;
+     public Texture[] textures;
+     
+     // Use this for initialization
+     void Start () {
+         StartCoroutine(DoTextureLoop());
+     }
+     
+     public IEnumerator DoTextureLoop(){
+         int i = 0;
+         while (true){
+             GetComponent<Renderer>().material.mainTexture = textures[i];
+             i = (i+1)%textures.Length;
+             yield return new WaitForSeconds(duration);
+         }
+     }
+ }
+
