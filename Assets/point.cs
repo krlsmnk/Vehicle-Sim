f37e56393b463c28e1380b70f40989b9343077eb
Karl Smink <krlsmnk@yahoo.com>
2020-03-16 12:09:57 -0500

    Point Cloud processing

---
+++ Assets/point.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class point : MonoBehaviour
+{
+    public double x, y, z, intensity, timestamp, ring, reflectivity, noise, range;
+
+    public point(double xVal, double yVal, double zVal, double inten, double tmst, double ringg, double refl, double ns, double rng)
+   {
+        x=xVal;
+        y=yVal;
+        z=zVal;
+        intensity = inten;
+        timestamp = tmst;
+        ring = ringg;
+        reflectivity = refl;
+        noise = ns;
+        range = rng;
+   }//end of constructor
+
+    //setters
+    public void Set_X(double value){x=value;}
+    public void Set_Y(double value){y=value;}
+    public void Set_Z(double value){z=value;}
+    public void Set_Intensity(double value){intensity=value;}
+    public void Set_Timestamp(double value){timestamp=value;}
+    public void Set_Ring(double value){ring=value;}
+    public void Set_Ref(double value){reflectivity=value;}
+    public void Set_Noise(double value){noise=value;}
+    public void Set_Rng(double value){range=value;}
+
+}//end of class
+
---
+++ Assets/pointCloud_generator.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+using System;
+
+public class pointCloud_generator : MonoBehaviour
+{
+    public TextAsset currentFrame;    
+    private int lineNum=0; //used to keep track of the header
+    private char[] delimiterChars = {','}; //characters which will be removed when parsing the file
+    private double[] pointVals = {0,0,0,0,0,0,0,0,0};
+    public List<point> pointCloud;
+
+    void Start()
+    {
+        string filePath = AssetDatabase.GetAssetPath(currentFrame);
+        readTextFile(filePath);
+    }//end of start
+
+
+    void readTextFile(string file_path)
+    {
+       StreamReader inp_stm = new StreamReader(file_path);
+       while(!inp_stm.EndOfStream)
+       {
+           //For each line in file:
+           string inp_ln = inp_stm.ReadLine( );
+           //Debug.Log(inp_ln);
+
+            bool skipPoint = false; //allows to skip data that isn't formatted correctly
+           
+            //if past header
+            if(lineNum > 9)
+            {
+                skipPoint = false;
+                //remove the "(" and ")" characters from the line
+                inp_ln = inp_ln.Replace("(", "");
+                inp_ln = inp_ln.Replace(")", "");
+                //inp_ln.Trim(new Char[] {'(', ')'} );
+                
+                //delimit by commas to get values
+                string[] splitLine = inp_ln.Split(delimiterChars);
+                if(splitLine.Length == 9) //skip data entry if not formatted correctly
+                {                
+                    //cast all values as doubles
+                    for(int i=0; i<9; i++){
+    //Debug.Log("Attempting to parse: " + splitLine[i]);
+                        try{
+                            pointVals[i] = Double.Parse(splitLine[i]);
+                        }
+                        catch
+                        {
+                            Debug.Log("Could not parse: " + splitLine[i] + " in slot: " + i + " on line: " + lineNum);
+                            skipPoint = true;
+                        }
+                    }
+                
+    //Debug.Log(pointVals);
+
+                    //avoid junk data
+                    if(pointVals[0] != 0 && skipPoint == false)
+                    {
+                        //make a point and add it to the cloud
+                        try
+                        {
+                            pointCloud.Add(new point(pointVals[0], pointVals[1], pointVals[2], pointVals[3], pointVals[4], pointVals[5], pointVals[6], pointVals[7], pointVals[8]));
+                        }
+                        catch
+                        {
+                            Debug.Log("Skipped making point: " + lineNum);
+                        }                   
+                    } 
+                }
+                
+            }
+
+        lineNum++; //increment line count
+       }//end of for each line
+       inp_stm.Close( );
+        
+       //draw cloud
+       drawCloud(pointCloud);
+
+    }//end of readTxt
+
+
+    private void drawCloud(List<point> pointCloud)
+    {
+        
+        Debug.Log(pointCloud.Count);
+
+    }//end of drawCloud
+
+
+}//end of class
+
